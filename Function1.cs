dc6bcca233cb73341016dfac209a4d8bf23b0bcb
Udith_Gayan <udith.indrakantha@gmail.com>
2023-02-16 01:46:21 +0530

    Added a durable function to add up the names passed

--- Function1.cs
+++ Function1.cs
@@ -7,29 +7,49 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using System.Collections.Generic;
+using System.Net.Http;
 
 namespace Http_Trigger
 {
     public static class Function1
     {
-        [FunctionName("Function1")]
+        [FunctionName("Http_Trigger")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req, [DurableClient] IDurableClient starter,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            Myself data = JsonConvert.DeserializeObject<Myself>(requestBody);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string instanceId = await starter.StartNewAsync("My_Orchestrator", data);
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
 
-            return new OkObjectResult(responseMessage);
+            return starter.CreateCheckStatusResponse(req, instanceId);
         }
+
+        [FunctionName("My_Orchestrator")]
+        public static async Task<string> MyOchestrator([OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            var obInput = context.GetInput<Myself>();
+            return await context.CallActivityAsync<string>("My_Activity", obInput.Name);
+        }
+
+        public static string newName = "";
+
+        [FunctionName("My_Activity")]
+        public static string MyActivity([ActivityTrigger] string myName, ILogger log)
+        {
+            newName += myName;
+            return newName;
+        }
+    }
+
+    class Myself
+    {
+        public string Name { get; set; }
     }
 }
